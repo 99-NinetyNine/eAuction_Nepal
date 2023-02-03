c06e873b9c848739d958255b14953d128557af28
sailesh <saileshshiwakoti2@gamil.com>
2023-02-03 22:54:19 +0545

    f

--- auctions/views.py
+++ auctions/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import(
     render,
     get_object_or_404,
+    redirect,
 ) 
+
     
 from django.urls import reverse_lazy,reverse
 from django.contrib.auth.decorators import login_required
