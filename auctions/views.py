7a4dac05119e562ce45fe380e89e571e66cc305f
Suraj Niroula <surajniraula321@gmail.com>
2023-02-24 22:41:08 +0545

    fixing error

--- auctions/views.py
+++ auctions/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import(
 ) 
 
     
+
+    
 from django.urls import reverse_lazy,reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import (
--- bidding/views.py
+++ bidding/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import(
     redirect,
 ) 
     
+    
 from django.urls import reverse_lazy,reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import (
